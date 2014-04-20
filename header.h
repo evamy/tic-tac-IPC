45e9e6ee7258fbf27de545893cb75bd2709a29d1
AntrikshA <AntrikshA@users.noreply.github.com>
2014-04-21 01:17:10 +0530

    Update header.h

--- header.h
+++ header.h
@@ -3,11 +3,20 @@
  all these functions are defined in header.c , the system will automatically link these files
 */
 
+#define GRN  "\x1B[32m"
+#define YEL  "\x1B[33m"
+#define NRM  "\x1B[0m"
+#define RED  "\x1B[31m"
+#define BLU  "\x1B[34m"
+
 typedef struct shared
 {
     char grid[3][3] ;
     int turn ;
-	int count;	// counts the no. of turns
+	int count;
+	int move;
+	int msgtype;
+	char msg[50];
 }shared;
 
 //function that will display the board which we have stored in shared memory
@@ -16,7 +25,8 @@ void display(shared* data);
 //function to initialize the grid with all blank spaces
 void initialize(shared* data);
 
-//function to check the winning condition , return 1 for success and 0 for failure
-int  check(shared* data);
 //function to clear the screen,yes i know how to do that
 void clear();
+
+//function to check the winning condition , return 1 for success and 0 for failure
+int  check(shared* data);
